5a06cb6a8c86fcc763ad4e362316ccd7bc5f5c20
Kazuki Ota <kaota@microsoft.com>
2020-11-16 23:49:45 +0900

    add regions

---
+++ Yubaba.Uno.Infrastructures/RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yubaba.Uno.Infrastructures
+{
+    public static class RegionNames
+    {
+        public static string LogRegion { get; } = nameof(LogRegion);
+        public static string ContractRegion { get; } = nameof(ContractRegion);
+    }
+}
